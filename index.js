a801522a224f0fe19163653fabcc6fd017acf6d2
MNATorres <mtri21019@gmail.com>
2023-04-26 20:31:50 -0300

    se cambia a node js

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post("/send-message", (req, res) => {
+  
+  const { name, email, phone, message } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: "mtri21019@gmail.com",
+      pass: "intheend007",
+    },
+  });
+
+  const mailOptions = {
+    from: email,
+    to: "your-email@gmail.com",
+    subject: `New message from ${name}`,
+    text: `
+      Name: ${name}
+      Email: ${email}
+      Phone: ${phone}
+      Message: ${message}
+    `,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      res.status(500).send("Error sending message");
+    } else {
+      res.status(200).send("Message sent successfully");
+    }
+  });
+});
+
+app.listen(3000, () => console.log("Server listening on port 3000"));
